fdb6b15d81d59e99107c5932da00f159269761a2
yoshibuchi <yoshibuchi.n@gmail.com>
2019-06-16 19:20:33 +0900

    first-commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+def create
+    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+    end
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :tweet_id)
+  end
