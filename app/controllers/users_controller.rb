0f1f74b9ac441c3aa0192ca4e8081bf47d6bd329
yoshibuchi <yoshibuchi.n@gmail.com>
2019-06-29 14:23:09 +0900

    AWS2-first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
 
     def index
-      @users = User.where('name LIKE(?)',"%#{params[:keyword]}%" ).where.not(:id current_user.id)
+      @users = User.where('name LIKE(?)',"%#{params[:keyword]}%" ).where.not(id: current_user.id)
       respond_to do |format|
         format.html
         format.json 
+      end
     end
-  end
+  
 
 
     def edit
@@ -25,4 +26,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email)
     end
-end
+  end
+
