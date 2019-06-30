986229d3a22182c12a6182a231c633d797a60d68
yoshibuchi <yoshibuchi.n@gmail.com>
2019-06-30 19:50:31 +0900

    shuusei-third

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,11 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit]
+  before_action :set_group, only: [:edit, :update]
     def new
       @group = Group.new
       @group.users << current_user
     end
 
     def create
-      # binding.pry
       @group = Group.new(group_params)
         if @group.save
           redirect_to root_path, notice: 'グループを作成しました'
